aff646cd6ec481ab805a9255008a5cd7e178d8a5
Mr. Bartucz <jobartucz@isd535.org>
2017-10-12 08:05:54 -0500

    Added tuples_and_lists.py

---
+++ tuples_and_lists.py
@@ -0,0 +1,16 @@
+# CSP 1.3.6 Stub file
+# [Put your name here]
+
+
+# [what does this function do?]
+def roll_two_dice():
+    
+    sum = 0
+    
+    return sum
+    
+
+# [what does this function do?]
+def guess_letter():
+    
+    return 'a'
